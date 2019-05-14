dcbf1c7c5e6c819c7e8759efda5626cf983f0070
Eduardo Ellery <eduardo.ellery@wirecard.com>
2019-05-14 11:32:20 +0200

    Adding shebang

--- git/check-local-merged-branches.sh
+++ git/check-local-merged-branches.sh
@@ -1,1 +1,3 @@
+#!/usr/bin/env sh
+
 git branch --merged | egrep -v "(^\*|master|staging|develop|dev|def)" | xargs -n 1 echo
--- git/check-merged-branches.sh
+++ git/check-merged-branches.sh
@@ -1,1 +1,3 @@
+#!/usr/bin/env sh
+
 git branch -r --merged | egrep -v "(^\*|master|staging|develop|dev|def)" | grep origin | sed 's/origin\///' | xargs -n 1 echo
