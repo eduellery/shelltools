d6e5e38226574153df25276ec744c36122da5430
Eduardo Ellery <eduardo.ellery@wirecard.com>
2019-05-14 11:59:44 +0200

    More git stuff

---
+++ git/create-patches-from-to-branch.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+FROM=$1
+TO=$2
+MERGE_BASE="$(git merge-base --octopus "$FROM" "$TO")"
+
+git format-patch $MERGE_BASE..$TO
---
+++ git/fetch-tags-by-date.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+git for-each-ref --sort=taggerdate --format '%(refname) %(taggerdate)' refs/tags
